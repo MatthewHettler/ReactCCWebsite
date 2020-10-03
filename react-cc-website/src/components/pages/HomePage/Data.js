4fcce3d7d9a1d2aa60f37aac46cbe5f9967f4bbe
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-10-03 18:58:42 -0500

    Added a bit more css and a const statment.

--- react-cc-website/src/components/pages/HomePage/Data.js
+++ react-cc-website/src/components/pages/HomePage/Data.js
@@ -11,3 +11,16 @@ export const homeObjOne = {
   img: "images/svg-1.svg",
   alt: "Credit Card",
 };
+
+export const homeObjFour = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'DATA ANALYTICS',
+  headline: 'Every transaction is stored on our secure cloud database',
+  description: 'Never ever have to worry again about saved receipts. We store your data, so you can access it anytime.',
+  buttonLabel: 'Sign Up Now',
+  imgStart: 'start',
+  img: 'images/svg-8.svg'
+  alt: 'Vault'
+}
