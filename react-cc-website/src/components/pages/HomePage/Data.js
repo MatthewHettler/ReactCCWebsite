7032447fc397effeb47717ecdb653ca2d2299f5c
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-10-05 09:34:35 -0500

    Added homeObjtwo to data.js.

--- react-cc-website/src/components/pages/HomePage/Data.js
+++ react-cc-website/src/components/pages/HomePage/Data.js
@@ -12,15 +12,30 @@ export const homeObjOne = {
   alt: "Credit Card",
 };
 
+export const homeObjTwo = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "100% SECURE",
+  headline: "Stay protected 24/7 anywhere anytime",
+  description:
+    "We have you covered no matter where you are located. Over 140 locations worldwide to ensure you have access anytime.",
+  buttonLabel: "Learn More",
+  imgStart: "",
+  img: "images/svg-5.svg",
+  alt: "Vault",
+};
+
 export const homeObjFour = {
   lightBg: false,
   lightText: true,
   lightTextDesc: true,
-  topLine: 'DATA ANALYTICS',
-  headline: 'Every transaction is stored on our secure cloud database',
-  description: 'Never ever have to worry again about saved receipts. We store your data, so you can access it anytime.',
-  buttonLabel: 'Sign Up Now',
-  imgStart: 'start',
-  img: 'images/svg-8.svg'
-  alt: 'Vault'
-}
+  topLine: "DATA ANALYTICS",
+  headline: "Every transaction is stored on our secure cloud database",
+  description:
+    "Never ever have to worry again about saved receipts. We store your data, so you can access it anytime.",
+  buttonLabel: "Sign Up Now",
+  imgStart: "start",
+  img: "images/svg-8.svg",
+  alt: "Vault",
+};
